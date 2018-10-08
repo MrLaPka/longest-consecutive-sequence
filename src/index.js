a080fe9fa8f5ffefa4efb6f32c62922409ebdf7f
Егор <42974925+MrLaPka@users.noreply.github.com>
2018-10-08 19:36:53 +0300

    10task_upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 module.exports = function longestConsecutiveLength(array) {
   let count = 1;
-  let arr = new Array;
   if(array.length == 0) return 0;
-  arr[0] = array[0];
-  let j = 0;
   function sort_numbers(a, b) {
     if (a > b)
     return 1;
@@ -13,20 +10,15 @@ module.exports = function longestConsecutiveLength(array) {
     return 0;
    } 
      array.sort(sort_numbers);
+     let max = 1;
 for(let i = 0;i<array.length;i++){
   if((array[i+1]-array[i]) == 1){
 count++;
-arr[j] = count;
   }
-  else if(((array[i+1]-array[i]) != 1)&&((array[i]-array[i-1]) == 1)){
+  else {
     count = 1;
-    j++;
   }
-  }
-  let max = arr[0];
-  for(let i = 0;i<arr.length;i++){
-    if(max<arr[i])
-    max = arr[i];
+  if(max<count) max = count;
   }
   return max;
 }
